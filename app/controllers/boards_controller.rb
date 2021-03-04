83e3865102b4f18ecb832a5814dcadda23c5dcb9
seki_yuts <>
2021-03-05 07:12:05 +0900

    board index, edit

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,24 @@
+class BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
+
+  def new
+    @board = current_user.boards.build
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to root_path, notice: '保存しました'
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
+  end
+end
