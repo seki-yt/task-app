8b08093cf765f9290e05e1e5a96a2feb6974556d
seki_yuts <>
2021-03-08 12:54:09 +0900

    update edit

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,10 @@
 class BoardsController < ApplicationController
+
+  before_action :authenticate_user!, only:[:edit, :update, :destroy]
+
+
   def index
     @boards = Board.all
-  end
-
-  def show
-    @board = Board.find(params[:id])
   end
 
   def new
@@ -19,6 +19,21 @@ class BoardsController < ApplicationController
       flash.now[:error] = '保存に失敗しました'
       render :new
     end
+  end
+
+  def edit
+    @board = current_user.boards.find(params[:id])
+  end
+
+  def update
+    @board = current_user.boards.find(params[:id])
+    if @board.update(board_params)
+      redirect_to root_path, notice: '更新しました。'
+    else
+      flash.now[:error] = '更新できませんでした。'
+      render :edit
+    end
+
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   root to: 'boards#index'
 
-  resources :boards
+  resources :boards, only: [:index, :new, :create, :edit, :update,:destroy,]
 
 end
