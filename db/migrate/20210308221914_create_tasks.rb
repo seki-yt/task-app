3099d9520317f32015b51569b43e7004b8313bd4
seki_yuts <>
2021-03-10 18:50:18 +0900

    task new

--- db/migrate/20210308221914_create_tasks.rb
+++ db/migrate/20210308221914_create_tasks.rb
@@ -2,6 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.references :user
+      t.references :board
       t.string :title
       t.text :content
       t.timestamps
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -7,11 +7,13 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  board_id   :bigint
 #  user_id    :bigint
 #
 # Indexes
 #
-#  index_tasks_on_user_id  (user_id)
+#  index_tasks_on_board_id  (board_id)
+#  index_tasks_on_user_id   (user_id)
 #
 require 'test_helper'
 
