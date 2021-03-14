e59a37febe9080c731274461bfbfc474e195138f
seki_yuts <>
2021-03-15 07:12:26 +0900

    no message

---
+++ app/models/comment.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  task_id    :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_comments_on_task_id  (task_id)
+#  index_comments_on_user_id  (user_id)
+#
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :task
+end
