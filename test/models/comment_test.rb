686de2cea78a9b2f9353273061f14843b080908a
seki_yuts <>
2021-03-15 07:11:56 +0900

    comment model create

---
+++ test/models/comment_test.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  task_id    :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_comments_on_task_id  (task_id)
+#  index_comments_on_user_id  (user_id)
+#
+require 'test_helper'
+
+class CommentTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
